f387884371b4631ae99d68d3e29b75b5000caa2e
Daniel Suruagy <dsuruagy@gmail.com>
2017-10-04 21:57:43 -0300

    Implementing naive bayes algorithm to predict emails authors

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,8 +26,29 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+def predict_author():
+    from sklearn.naive_bayes import GaussianNB
+    clf = GaussianNB()
 
+    t0 = time()
+    clf.fit(features_train, labels_train)
+    # training time is around 1.414 seconds
+    print "training time:", round(time() - t0, 3), "s"
 
+    t1 = time()
+    pred = clf.predict(features_test)
+    # predicting time is around 0.212 s
+    print "predicting time:", round(time() - t1, 3), "s"
+
+    return pred
+
+def verify_accuracy(predict, labels):
+    from sklearn.metrics import accuracy_score
+
+    return accuracy_score(labels, predict)
+
+accuracy = verify_accuracy(labels_test, predict_author())
+print 'accuracy:', accuracy
 #########################################################
 
 
