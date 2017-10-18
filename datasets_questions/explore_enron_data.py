b753befd82ea45671b73564621e3d2fc6860b4e4
Daniel Suruagy <dsuruagy@gmail.com>
2017-10-17 23:02:02 -0300

    Lesson 6 exercises added

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -27,9 +27,40 @@ total_poi = 0
 for data in enron_data:
 	if print_feature:
 		feature  = enron_data[data]
-		print 'each person\'s features:', len(feature)
+		print '\neach person\'s features:', len(feature)
 		print_feature = False
+		print feature
 	if enron_data[data]['poi']:
 		print 'POI:', data
 		total_poi += 1
-print 'number of persons of interest:', total_poi
+print '\nnumber of persons of interest:', total_poi
+
+print '\nJames Prentice\'s value of stock:', enron_data['PRENTICE JAMES']['total_stock_value']
+
+print '\nWesley Colwell\'s emails to POI:', enron_data['COLWELL WESLEY']['from_this_person_to_poi']
+
+print '\nJeffrey Skilling\'s exercised_stock_options:', \
+    enron_data['SKILLING JEFFREY K']['exercised_stock_options']
+
+print '\nLAY KENNETH L total_payments:', enron_data['LAY KENNETH L']['total_payments']
+print 'FASTOW ANDREW S total_payments:', enron_data['FASTOW ANDREW S']['total_payments']
+print 'SKILLING JEFFREY K total_payments:', enron_data['SKILLING JEFFREY K']['total_payments']
+
+
+#print 'total with quantified salary', len(enron_data[enron_data.keys()]['salary' != 'NaN'])
+#values = enron_data.values()
+with_salary = {}
+with_email = {}
+wo_total_pay = {}
+for value in enron_data:
+	if enron_data[value]['salary'] != 'NaN':
+		with_salary[value] = enron_data[value]
+	if enron_data[value]['email_address'] != 'NaN':
+		with_email[value] = enron_data[value]
+	if enron_data[value]['total_payments'] == 'NaN':
+		wo_total_pay[value] = enron_data[value]
+print '\ntotal with quantified salary:', len(with_salary)
+print 'total with email address:', len(with_email)
+print '\npercent with email address:', (len(wo_total_pay) / float(len(enron_data)))
+
+
