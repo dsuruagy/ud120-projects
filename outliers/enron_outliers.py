cea0642d5babb990591c79d629a7994b103777a6
Daniel Suruagy <dsuruagy@gmail.com>
2017-10-21 12:32:17 -0300

    Added code to identify if outliers are of interest or not.

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -9,11 +9,27 @@ from feature_format import featureFormat, targetFeatureSplit
 
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+
+# removing TOTAL data_point, because it's an outlier:
+data_dict.pop('TOTAL')
+
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
 
 ### your code below
+#print "data_dict keys:", sorted(data_dict.keys())
+for person in data_dict:
+	p = data_dict[person]
+	if p['salary'] != 'NaN' and p['salary'] > 1000000 and \
+		p['bonus'] != 'NaN' and p['bonus'] > 5000000:
+		print person
 
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
 
-
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
