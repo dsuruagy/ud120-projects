140a840fd98c39c8e3d198d86b51a61df4e0eca8
Daniel Suruagy <dsuruagy@gmail.com>
2017-11-13 23:03:03 -0300

    Use of SelectFromModel for feature selection, and testing some classifiers.

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -11,10 +11,11 @@ from tester import dump_classifier_and_data
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
 # You will need to use more features
-features_list = ['poi','salary', 'deferral_payments', 'total_payments', 'loan_advances', 'bonus', \
-                   'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses',\
-                   'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock',\
-                   'director_fees']
+features_list = ['poi','salary', 'deferral_payments', 'total_payments', 'loan_advances', 'bonus',
+                   'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses',
+                   'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock',
+                   'director_fees', 'to_messages', 'from_poi_to_this_person', 'from_messages',
+                   'from_this_person_to_poi', 'shared_receipt_with_poi']
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
@@ -24,9 +25,9 @@ with open("final_project_dataset.pkl", "r") as data_file:
 import pandas as pd
 df = pd.DataFrame(data_dict).transpose()
 
-financial_feats = ['salary', 'deferral_payments', 'total_payments', 'loan_advances', 'bonus', \
-                   'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses',\
-                   'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock',\
+financial_feats = ['salary', 'deferral_payments', 'total_payments', 'loan_advances', 'bonus',
+                   'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses',
+                   'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock',
                    'director_fees', 'poi']
 df[financial_feats] = df[financial_feats].apply(pd.to_numeric, errors ='coerce')
 #print df.corr()['poi']
@@ -37,8 +38,19 @@ df[financial_feats] = df[financial_feats].apply(pd.to_numeric, errors ='coerce')
 my_dataset = data_dict
 
 ### Extract features and labels from dataset for local testing
-data = featureFormat(my_dataset, features_list, sort_keys = True, remove_NaN=False)
+data = featureFormat(my_dataset, features_list, sort_keys = True, remove_NaN=True)
 labels, features = targetFeatureSplit(data)
+
+### Doing Feature Selection
+print 'features.shape:', features[0]
+
+from sklearn.feature_selection import SelectFromModel
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression().fit(features, labels)
+model = SelectFromModel(lin_reg, prefit=True, threshold='0.008*mean')
+features = model.transform(features)
+
+print 'new features.shape:', features[0]
 
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
@@ -47,9 +59,22 @@ labels, features = targetFeatureSplit(data)
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
 # Provided to give you a starting point. Try a variety of classifiers.
-#from sklearn.naive_bayes import GaussianNB
-#clf = GaussianNB()
+#Accuracy: 0.37620	Precision: 0.15599	Recall: 0.83400	F1: 0.26282	F2: 0.44616
+#Total predictions: 15000	True positives: 1668	False positives: 9025	False negatives:  332	True negatives: 3975
 
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+
+#Accuracy: 0.78860	Precision: 0.19897	Recall: 0.19350	F1: 0.19620	F2: 0.19457
+#Total predictions: 15000	True positives:  387	False positives: 1558	False negatives: 1613	True negatives: 11442
+
+#from sklearn.tree import DecisionTreeClassifier
+#clf = DecisionTreeClassifier()
+
+
+#Accuracy: 0.85427	Precision: 0.24451	Recall: 0.04450	F1: 0.07530	F2: 0.05320
+#Total predictions: 15000	True positives:   89	False positives:  275	False negatives: 1911	True negatives: 12725
+'''
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 param_grid = {
@@ -59,8 +84,20 @@ param_grid = {
 	'random_state' : [5, 7]}
 
 clf = GridSearchCV(RandomForestClassifier(), param_grid=param_grid, n_jobs=-1)
+'''
 
-
+#Accuracy: 0.83633	Precision: 0.20945	Recall: 0.08200	F1: 0.11786	F2: 0.09336
+#Total predictions: 15000	True positives:  164	False positives:  619	False negatives: 1836	True negatives: 12381
+'''
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+clf = AdaBoostClassifier(
+	base_estimator = DecisionTreeClassifier(max_depth = 2),
+	n_estimators = 50,
+	learning_rate = 0.03,
+	algorithm = 'SAMME.R',
+	random_state = 6)
+'''
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
 ### folder for details on the evaluation method, especially the test_classifier
