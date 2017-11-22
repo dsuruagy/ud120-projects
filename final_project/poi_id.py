cd87f35b1ace2c0be247828e291ac46626845d2d
Daniel Suruagy <dsuruagy@gmail.com>
2017-11-21 22:42:33 -0300

    Adjustments based on live help with instructor. Using scaler and feature selection on Pipeline.

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -4,9 +4,9 @@ import sys
 import pickle
 sys.path.append("../tools/")
 
-import poi_id_utils as pu
+#import poi_id_utils as pu
 from feature_format import featureFormat, targetFeatureSplit
-from tester import dump_classifier_and_data, main
+from tester import dump_classifier_and_data
 
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
@@ -23,13 +23,13 @@ with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
 ### Task 2: Remove outliers
-pu.describe_data(data_dict)
+#pu.describe_data(data_dict)
 
 # removing TOTAL data_point, because it's an outlier:
 data_dict.pop('TOTAL')
 
 # converting data dict to dataframe, to use statistics functions
-df = pu.datadict_to_dataframe(data_dict, features_list)
+#df = pu.datadict_to_dataframe(data_dict, features_list)
 
 # with the describe function, it's possible to see if there are other max values for outliers
 # like for the TOTAL observation
@@ -44,21 +44,9 @@ my_dataset = data_dict
 data = featureFormat(my_dataset, features_list, sort_keys = True, remove_NaN=True)
 labels, features = targetFeatureSplit(data)
 
-
-# print features' correlation with poi
-#pu.poi_correlation(data_dict, features_list)
-
 # Feature selecting
-from sklearn.preprocessing import scale
-feat_scale = scale(features)
-
-from sklearn.svm import LinearSVC
-features_list = pu.select_features(LinearSVC(C=0.1, penalty="l1", dual=False), labels, feat_scale, features_list)
-
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import f_classif
-anova_filter = SelectKBest(f_classif, k=5)
-#features_list = anova_filter.fit_transform(feat_scale, labels)
+#from sklearn.preprocessing import scale
+#feat_scale = scale(features)
 
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
@@ -70,11 +58,11 @@ if True:
     from sklearn.tree import DecisionTreeClassifier
     classif = DecisionTreeClassifier()
 
-    param_grid = {
+    param_grid = {'skb__k' : range(4, 8),
               'classifier__criterion' : ('gini', 'entropy'),
               'classifier__splitter' : ('best', 'random'),
               'classifier__max_depth' : [None, 1, 2, 3],
-              'classifier__random_state' : range(20,42)}
+              'classifier__random_state' : [31,42]}
 
 
 if False:
@@ -82,19 +70,21 @@ if False:
     from sklearn.ensemble import AdaBoostClassifier
     classif = AdaBoostClassifier(base_estimator = DecisionTreeClassifier())
 
-    param_grid = {'reduce_dim__n_components' : [None, 1, 19],
+    param_grid = {'skb__k' : range(4, 8),
+              'reduce_dim__n_components' : [None, 1],
               'classifier__base_estimator__criterion' : ('gini', 'entropy'),
               'classifier__base_estimator__splitter' : ('best', 'random'),
               'classifier__base_estimator__max_depth' : [None, 1, 2],
               'classifier__algorithm' : ('SAMME', 'SAMME.R'),
-              'classifier__random_state' : range(30,42)}
+              'classifier__random_state' : [31, 42]}
 
 
 if False:
     from sklearn.ensemble import RandomForestClassifier
     classif = RandomForestClassifier()
 
-    param_grid = {'reduce_dim__n_components' : [None, 1, 19],
+    param_grid = {'skb__k' : range(4, 8),
+            'reduce_dim__n_components' : [None, 1],
             'classifier__n_estimators': [1, 10, 40],
             'classifier__max_depth': [None, 1, 2],
             'classifier__criterion': ('gini', 'entropy'),
@@ -113,28 +103,32 @@ from sklearn.pipeline import Pipeline
 from sklearn.decomposition import PCA
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.feature_selection import SelectKBest
 
 sss = StratifiedShuffleSplit(n_splits=10, test_size=0.3, random_state=42)
-
-estimators = [('reduce_dim', PCA()), ('classifier', classif)]
+estimators = [('scaler', MinMaxScaler()), ('skb', SelectKBest()), ('reduce_dim', PCA()), ('classifier', classif)]
 pipe = Pipeline(estimators)
-clf = GridSearchCV(pipe, param_grid=param_grid, n_jobs=-1, cv=sss, scoring="f1")
-
-# Example starting point. Try investigating other evaluation techniques!
-from sklearn.model_selection import train_test_split
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(feat_scale, labels, test_size=0.3, random_state=42)
-
-clf.fit(features_train, labels_train)
-clf = clf.best_estimator_
+gs = GridSearchCV(pipe, param_grid=param_grid, n_jobs=-1, cv=sss, scoring="f1")
+gs.fit(features, labels)
+clf = gs.best_estimator_
 print clf
 
-from sklearn.metrics import accuracy_score, precision_score, recall_score
 
-predict = clf.predict(features_test)
-print 'accuracy:', accuracy_score(labels_test, predict)
-print 'precision:', precision_score(labels_test, predict)
-print 'recall:', recall_score(labels_test, predict)
+## discovering the selected features from SelectKBest:
+skb = clf.get_params()['skb']
+feature_idx = skb.get_support()
+
+### removing the first feature (poi), to match selected indexes
+import numpy as np
+poi = features_list.pop(0)
+features_np = np.array(features_list)[feature_idx]
+print '\nselected features:', features_np
+
+### reinserting poi feature, needed to execute the test
+features_list = np.insert(features_np, 0, poi)
+
+
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
